036db1338865f3c242b9dc80a291654c6514bba6
heidiyee <heidi.n.yee@gmail.com>
2015-11-23 12:51:57 -0800

    First commit

---
+++ LocationCF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationCF
+//
+//  Created by Heidi Yee on 11/23/15.
+//  Copyright © 2015 Heidi Yee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
